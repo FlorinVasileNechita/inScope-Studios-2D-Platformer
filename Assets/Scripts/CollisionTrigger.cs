571e951db7ba4d70f8021460ece05a69605770d6
Joe <governmentslave@hotmail.com>
2016-02-10 19:08:17 -0500

    Add one way small and medium platforms

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTrigger : MonoBehaviour {
+
+    private BoxCollider2D playerCollider;
+
+    [SerializeField]
+    private BoxCollider2D platformCollider;
+    [SerializeField]
+    private BoxCollider2D platformTrigger;
+
+	// Use this for initialization
+	void Start () {
+        // So ugly
+        playerCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true);
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Player") {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.name == "Player") {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, false);
+        }
+    }
+}
