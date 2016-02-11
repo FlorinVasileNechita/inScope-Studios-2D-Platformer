6487ce7d63640879e546bad56af3beff6f716126
Joe <governmentslave@hotmail.com>
2016-02-11 10:41:54 -0500

    Video 13 - Add camera follow, level decoration, and crate prefab

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private float xMax, yMax;
+    [SerializeField]
+    private float xMin, yMin;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player").transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void LateUpdate() {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+    }
+}
