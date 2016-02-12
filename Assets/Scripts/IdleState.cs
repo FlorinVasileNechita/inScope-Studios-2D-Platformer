8054e55899974b3b5e04e9232a52b9235f233b68
sircuddles <governmentslave@hotmail.com>
2016-02-12 13:18:46 -0500

    Video 16.2 - Implement enemy state machine base

---
+++ Assets/Scripts/IdleState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleState : IEnemyState {
+
+    public void Execute() {
+
+    }
+
+    public void Enter(Enemy enemy) {
+
+    }
+
+    public void Exit() {
+
+    }
+
+    public void OnTriggerEnter(Collider2D other) {
+
+    }
+}
